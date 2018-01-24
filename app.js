bec421c5c40591877ae355c6fc58c6a20f8c5116
nidamydyti <nidamydyti@gmail.com>
2018-01-24 14:16:30 +0100

    CRUD routes created, form frontend added

--- app.js
+++ app.js
@@ -8,11 +8,51 @@ app.set('views',__dirname + '/views');
 //set template engine
 app.set('view engine', 'ejs');
 
+//setup mongodb
+const MongoClient = require('mongodb').MongoClient;
+const mongoURL = 'mongodb://localhost:27017/todolist';
+
+//conneting to Mongodb
+MongoClient.connect(mongoURL, function(err, db) {
+	if (err) {
+		console.log(err);
+	}else {
+		console.log('Database conenected');
+		todos = db.collection('todos');
+	}
+});
+
+
+
 
 //routes
 app.get('/', function(req, res){
-	res.render("index", {name: "faqja fillestare", title:"index"});
+	res.render("index");
 });
+
+app.get('/todos/:id', function(req, res){
+	res.render("show"); 
+});
+app.post('/todos/add', function(req, res){
+	res.redirect("/");
+});
+
+app.get('/todos/edit/:id', function(req, res){
+	res.render("edit");
+});
+
+app.post('/todos/update/:id', function(req, res){
+	res.redirect("/");
+});
+
+app.get('/todos/delete/:id', function(req, res){
+	res.redirect("/");
+});
+
+/*
+app.get('/manage', function(req, res){
+	res.render('manage');
+});*/
 
 //runing app
 app.listen(3000, function(){
