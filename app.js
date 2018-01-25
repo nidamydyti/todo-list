b62c7f5d70253bd0101957403cbe3c581451be49
nidamydyti <nidamydyti@gmail.com>
2018-01-25 10:29:14 +0100

    read, update, delete of todos

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ app.set('views',__dirname + '/views');
 //set template engine
 app.set('view engine', 'ejs');
 
+//body parser middleware
+var bodyParser = require('body-parser');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+
+
 //setup mongodb
 const MongoClient = require('mongodb').MongoClient;
 const mongoURL = 'mongodb://localhost:27017/todolist';
-
+const ObjectId = require('mongodb').ObjectId;
 //conneting to Mongodb
 MongoClient.connect(mongoURL, function(err, db) {
 	if (err) {
@@ -25,28 +36,90 @@ MongoClient.connect(mongoURL, function(err, db) {
 
 
 
-//routes
+//routes   /-reed all
 app.get('/', function(req, res){
-	res.render("index");
+
+   todos.find().toArray(function(err, docs) {
+   	if(err) {
+   		console.log(err);
+   	}else {
+   			res.render("index", {docs: docs});
+   	}
+   });
+
+
+});
+///todos/:id nje document
+app.get('/todos/:id', function(req, res){
+var id = ObjectId(req.params.id);
+
+todos.findOne({_id: id}, function(err, doc) {
+	if(err){
+		console.log(err);
+	}else {
+		res.render("show", {doc: doc});
+	}
 });
 
-app.get('/todos/:id', function(req, res){
-	res.render("show"); 
+	 
 });
+
+
+// /todos/add'-create insert
 app.post('/todos/add', function(req, res){
-	res.redirect("/");
+	todos.insert({title: req.body.title, description: req.body.description}, function(err, result) {
+		if (err) {
+			console.log(err);
+		} else{
+			res.redirect("/");
+		}
+	} );
+	
 });
 
 app.get('/todos/edit/:id', function(req, res){
-	res.render("edit");
+  
+  var id = ObjectId(req.params.id);
+
+  todos.findOne({_id: id}, function(err, doc){
+  	if(err) {
+  		console.log(err);
+
+  	}else{
+
+  		res.render("edit", {doc: doc});
+  	}
+  });	
 });
+
+
 
 app.post('/todos/update/:id', function(req, res){
-	res.redirect("/");
+   var id = ObjectId(req.params.id);
+
+   todos.updateOne({_id:id}, {$set: {title: req.body.title, description: req.body.description}  }, function(err, result) {
+   	if(err) {
+   		console.log(err);
+
+   	}else{
+   		res.redirect("/");
+   	}
+   } );
+
+	
 });
 
+
 app.get('/todos/delete/:id', function(req, res){
-	res.redirect("/");
+	var id = ObjectId(req.params.id);
+	todos.deleteOne({_id:id}, function(err, result) {
+		if(err) {
+			console.log(err);
+		}else {
+			res.redirect("/");
+		}
+	});
+	
 });
 
 /*
